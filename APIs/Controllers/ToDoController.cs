c49aad6a4f3bd3dd4e5c577d04f72e72644b2c7c
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2022-02-28 15:34:38 +0200

    Add ToDoController.cs

---
+++ APIs/Controllers/ToDoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+    }
+}
