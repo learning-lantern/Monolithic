f0a7ad4e959ae922ad342291bc090d9aa0a347f7
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2022-03-24 17:26:54 +0200

    Add ClassroomController

---
+++ API/Classroom/Controllers/ClassroomController.cs
@@ -0,0 +1,19 @@
+﻿using API.Classroom.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Classroom.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassroomController : ControllerBase
+    {
+        private readonly IClassroomRepository classroomRepository;
+
+        public ClassroomController(IClassroomRepository classroomRepository)
+        {
+            this.classroomRepository = classroomRepository;
+        }
+
+
+    }
+}
