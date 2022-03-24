f426266e13550e1bed67127e693b246bb2e6650b
Omnia1Ahmed <omnia259ahmed@gmail.com>
2022-03-24 06:14:51 +0200

    Quiz Api

---
+++ Quiz/Controllers/QuizController.cs
@@ -0,0 +1,85 @@
+﻿using API.Quiz.DTOs;
+using API.Quiz.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API.Quiz.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController, Authorize]
+    public class QuizController : ControllerBase
+    {
+        private readonly IQuizRepository quizRepository;
+
+        public QuizController(IQuizRepository quizRepository)
+        {
+            this.quizRepository = quizRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string InstructorId)
+        {
+            var quizs = await quizRepository.GetAsync(InstructorId);
+
+            return Ok(JsonConvert.SerializeObject(quizs));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] AddQuizDTO addQuizDTO)
+        {
+            var addAsyncResult = await quizRepository.AddAsync(addQuizDTO);
+
+            if (addAsyncResult is null)
+            {
+                return NotFound(JsonConvert.SerializeObject(
+                "There is no user in this University with this Id."));
+            }
+            if (addAsyncResult.Value > 0)
+            {
+                return Ok(JsonConvert.SerializeObject(new QuizDTO(addQuizDTO) { Id = addAsyncResult.Value }));
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] QuizDTO quizDTO)
+        {
+            var updateAsyncResult = await quizRepository.UpdateAsync(quizDTO);
+
+            if (updateAsyncResult is null)
+            {
+                return NotFound(JsonConvert.SerializeObject(
+                "There is no user in this University with this Id."));
+            }
+
+            if (updateAsyncResult.Value)
+            {
+                return Ok(JsonConvert.SerializeObject(quizDTO));
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Remove([FromQuery] int quizId)
+        {
+            var removeAsyncResult = await quizRepository.RemoveAsync(quizId);
+
+            if (removeAsyncResult is null)
+            {
+                return NotFound(JsonConvert.SerializeObject(
+                "There is no user in this University with this Id."));
+            }
+
+            if (removeAsyncResult.Value)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    
+    }
+}
---
+++ Quiz/DTOs/QuizDTO.cs
@@ -0,0 +1,24 @@
+﻿using API.Quiz.Models;
+using API.User.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Quiz.DTOs
+{
+    public class QuizDTO : AddQuizDTO
+    {
+        [Required, Key]
+        public int Id { get; set; }
+        
+
+        public QuizDTO() { } 
+        public QuizDTO(AddQuizDTO addQuizDTO) : base(addQuizDTO)
+        {
+            
+
+        }
+        public QuizDTO(QuizDTO quizDTO, UserModel userModel) : base(quizDTO) {
+            Id = quizDTO.Id;
+        }
+
+    }
+}
