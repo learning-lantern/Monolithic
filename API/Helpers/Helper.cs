4e695238efeae22524f77f373737ff27f30fc25b
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2022-03-26 02:16:21 +0200

    Add some helpers classes and remove appsettings.json

---
+++ API/Helpers/Helper.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace API.Helpers
+{
+    public static class Helper
+    {
+        public static readonly Regex PasswordRegex = new(Pattern.Password);
+
+        public static readonly SmtpClient smtpClient = new(host: Message.FromEmailHost, port: 587)
+        {
+            EnableSsl = true,
+            Credentials = new NetworkCredential(
+                userName: Message.FromEmail,
+                password: Message.FromEmailPassword)
+        };
+
+        private static readonly List<string> universities = new()
+        {
+            "Assiut University"
+        };
+
+        public static bool IsUniversityValid(string universityName)
+        {
+            foreach (var university in universities)
+            {
+                if (university == universityName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IsNameValid(string name) => name.Replace(" ", "").Length >= 2;
+    }
+}
